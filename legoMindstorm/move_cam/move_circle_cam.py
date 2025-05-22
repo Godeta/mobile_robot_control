f5ce8a78a27810064016178a76a0f3ad5dae39ab
Godeta <Godeta61@gmail.com>
2025-05-22 02:29:59 +0200

    programme detection objet approche arc de cercle fonctionnel plus qu'a regler l'arrivée selon l'objet et peut être faire plus d'incrémentations

--- legoMindstorm/move_cam/move_circle_cam.py
+++ legoMindstorm/move_cam/move_circle_cam.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveDifferential, SpeedRPM
+from ev3dev2.sensor import Sensor, INPUT_1
 from ev3dev2.display import Display
 from ev3dev2.wheel import EV3Tire
+from ev3dev2.port import LegoPort
 import time
+
+# Setup Pixy camera
+LegoPort(INPUT_1).mode = 'auto'
+time.sleep(2)
+pixy = Sensor(INPUT_1)
+pixy.mode = 'ALL'
 
 # https://www.brickowl.com/catalog/lego-mindstorms-ev3-set-31313/inventory pièce 44309 doc code https://ev3dev-lang.readthedocs.io/projects/python-ev3dev/en/stable/motors.html#large-ev3-motor
 # Initialize the display
@@ -12,8 +20,29 @@ lcd.clear()
 lcd.text_pixels("Robot Navigation Program", False, 0, 0)
 lcd.update()
 
+# Variable to track the detected signature
+current_signature = 0
+saved_signature = 0
+is_scanning = False  # Flag to control when camera is actively scanning
+
+def get_signature():
+    """
+    Get the current signature detected by Pixy camera
+    Returns signature number (1-3) or 0 if none detected
+    """
+    try:
+        # In 'ALL' mode, the first value indicates signature number
+        signature = pixy.value(0)
+        
+        # Check if it's one of our target signatures (1, 2, or 3)
+        if signature in [1, 2, 3]:
+            return signature
+        return 0
+    except Exception:
+        return 0
+
 # Set up the robot parameters
-# - Uses custom 40mm diameter wheels
+# - Uses standard lego wheels
 # - Wheels are 125mm apart
 wheel_distance_mm = 125
 
@@ -42,12 +71,33 @@ try:
     # Drive for 200 mm around this imaginary circle
     lcd.text_pixels("Driving in arc...", False, 0, 30)
     lcd.update()
-    # robot.on_arc_right(SpeedRPM(-40), arc_radius, perimeter)
-    robot.on_arc_right(SpeedRPM(-40), arc_radius, perimeter/2)
-    # stop avant et détection couleur, si j'en vois une puis que je la perds de vue alors je l'ai, je tourne
-    robot.on_arc_right(SpeedRPM(-40), arc_radius, perimeter/4)
-    robot.on_arc_right(SpeedRPM(-40), arc_radius, perimeter/4)
+
+    # stop avant et détection couleur
+    inc_dist = 200
+    add_inc_dist = inc_dist/5
+    cumul = 0
+    insideDetect = False # vrai si on est passé à une valeur autre que 0 puis de retour à 0
+    robot.on_arc_right(SpeedRPM(-40), arc_radius, perimeter-inc_dist)
+    has_seen_object = False
+    while cumul < inc_dist and not insideDetect:
+        cumul += add_inc_dist
+        #check cam signature
+        current_signature = get_signature()
+        if current_signature != 0 and has_seen_object == False:
+                saved_signature = current_signature
+                has_seen_object = True
+        elif current_signature != saved_signature :
+                insideDetect = True
+        lcd.update()
+        lcd.text_pixels("Signature: ", True, 0, 0)
+        lcd.text_pixels(str(saved_signature), False, 89, 34)
+        lcd.text_pixels("Inside detect : ", False, 89, 48)
+        lcd.text_pixels(str(insideDetect), False, 89, 60)
+        # go forward
+        robot.on_arc_right(SpeedRPM(-40), arc_radius, add_inc_dist)
     
+    # if save_sign...
+
     # Rotate 90 degrees clockwise
     robot.turn_right(SpeedRPM(-40), 90)
     # straight forward
